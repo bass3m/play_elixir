fe20425c23c5d5e20b0d54ef27994dde55460877
Bassem Youssef <bassem@outlook.com>
2014-06-07 09:24:27 -0700

    simple binary search

---
+++ lib/bsearch.ex
@@ -0,0 +1,33 @@
+defmodule Bsearch do
+  @moduledoc """
+  simple binary search
+  """
+
+  #data : list of sorted numbers
+  #target : number we're looking for
+  #low : current low index
+  #high : current high index
+  defp bsearch(data,target,low,high) when low <= high do
+    mid = (low + high)/2 |> Float.floor
+    cond do
+      Enum.at(data,mid) > target ->
+        bsearch(data,target,low,mid-1)
+      Enum.at(data,mid) < target ->
+        bsearch(data,target,mid+1,high)
+      Enum.at(data,mid) == target ->
+        IO.puts("Found target #{target} at: #{mid}")
+    end
+  end
+
+  defp bsearch(data,target,low,high) do
+    IO.puts("Not Found target #{target} in: #{inspect data}")
+  end
+
+  @doc """
+  data : list of sorted numbers
+  target : number we're looking for
+  """
+  def bsearch(data,target) do
+    bsearch(data,target,0,length(data)-1)
+  end
+end
