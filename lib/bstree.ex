0e900ca214f0ac8921799fef780399c26300bfb6
Bassem Youssef <bassem@outlook.com>
2014-06-08 09:04:44 -0700

    Add min/max_depth and lowest common ancestor

--- lib/bstree.ex
+++ lib/bstree.ex
@@ -7,8 +7,9 @@ defmodule BSTree do
   t3 = BSTree.add_entry(t2,3)
   t4 = BSTree.add_entry(t3,0)
   BSTree.traverse(:inorder,t4)
-  Another tree:
-  List.foldl([3,10,1,6,14,4,7],BSTree.add_entry(8),
+  Or more elixir like, another tree:
+  List.foldl([3,10,1,6,14,4,7],
+             BSTree.add_entry(8),
              fn (x,acc) -> BSTree.add_entry(acc,x) end)
   """
   defstruct item: nil, left: nil, right: nil
@@ -48,6 +49,36 @@ defmodule BSTree do
     traverse(:inorder,left)
     IO.puts("Item : #{inspect item}")
     traverse(:inorder,right)
+  end
+
+  def min_depth(tree) when tree == nil, do: 0
+  def min_depth(%BSTree{left: left, right: right} = _tree) do
+    1 + min(min_depth(left),min_depth(right))
+  end
+
+  def max_depth(tree) when tree == nil, do: 0
+  def max_depth(%BSTree{left: left, right: right} = _tree) do
+    1 + max(min_depth(left),min_depth(right))
+  end
+
+  @doc """
+  Find the lowest common ancestor for a tree giving two node values
+  t3 = List.foldl([8,22,4,12,10,14],BSTree.add_entry(20),
+                  fn (x,acc) -> BSTree.add_entry(acc,x) end)
+  BSTree.lca(t3,10,14) => 12
+  BSTree.lca(t3,14,8) => 8
+  BSTree.lca(t3,10,22) => 20
+  """
+  def lca(%BSTree{left: left, right: _right, item: item} = _tree,
+          item1,item2) when item1 < item and item2 < item do
+    lca(left,item1,item2)
+  end
+  def lca(%BSTree{left: _left, right: right, item: item} = _tree,
+          item1,item2) when item1 > item and item2 > item do
+    lca(right,item1,item2)
+  end
+  def lca(%BSTree{item: item},item1,item2) do
+    IO.puts("Least common ancestor for #{item1} and #{item2} Node #{item}")
   end
 
 end
