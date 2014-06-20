fa75d85721667f5f4d5f1a6e51476b1e1e67c936
Bassem Youssef <bassem@outlook.com>
2014-06-20 08:02:18 -0700

    macros

---
+++ lib/macros.ex
@@ -0,0 +1,20 @@
+defmodule Macros do
+  @moduledoc """
+  A couple of assert macros
+  have to import Macros from iex to use
+  Macros.my_assert 1 == 2
+  Macros.my_assert 1 == 1
+  """
+  defmacro my_assert({op,_,[lhs,rhs]}) do
+    quote do
+      do_my_assert(unquote(op),unquote(lhs),unquote(rhs))
+    end
+  end
+
+  def do_my_assert(:==,lhs,rhs) when lhs == rhs do
+    IO.puts("==")
+  end
+  def do_my_assert(:==,lhs,rhs) do
+    IO.puts("!== #{lhs} and #{rhs}")
+  end
+end
