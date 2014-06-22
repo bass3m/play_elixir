bb32ab8a0503e97bc5ded1ab7e46192598b30d90
Bassem Youssef <bassem@outlook.com>
2014-06-22 11:23:05 -0700

    parens balancing

---
+++ lib/balanced_parens.ex
@@ -0,0 +1,18 @@
+defmodule BalanceParens do
+  @moduledoc """
+  Returns whether a string has a balanced number
+  of parenthesis
+  """
+  def balance(str) do
+    balance(str,0)
+  end
+  def balance(str,acc) when str == "", do: acc == 0
+  def balance(str,acc) do
+    [h | [t]] = String.split(str,"",global: false)
+    case h do
+      "(" -> balance(t,acc + 1)
+      ")" -> balance(t,acc - 1)
+      _ -> balance(t,acc)
+    end
+  end
+end
