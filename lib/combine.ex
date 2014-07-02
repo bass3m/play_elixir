41673398c7196aa6a8af21147be2febf297a9343
Bassem Youssef <bassem@outlook.com>
2014-07-02 07:42:00 -0700

    better docs

--- lib/combine.ex
+++ lib/combine.ex
@@ -2,6 +2,12 @@ defmodule Combine do
   @moduledoc """
   Combinations:
   given a non-negative integer n, return all size n combinations
+  Combine.comb(2,[1,2,3])
+  [[1, 2], [1, 3], [2, 3]]
+  """
+
+  @doc """
+  comb(combination_size, list)
   """
   def comb(0,_), do: [[]]
   def comb(_,[]), do: []
