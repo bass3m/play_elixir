b45338b00f725ac937251dc9729d7840287f6192
Bassem Youssef <bassem@outlook.com>
2014-07-03 23:11:08 -0700

    formatting

--- lib/combine.ex
+++ lib/combine.ex
@@ -2,10 +2,10 @@ defmodule Combine do
   @moduledoc """
   Combinations:
   given a non-negative integer n, return all size n combinations
-  Combine.comb(2,[1,2,3])
-  [[1, 2], [1, 3], [2, 3]]
-  Combine.comb(3,1..5 |> Enum.into([]))
-  [[1, 2, 3], [1, 2, 4], [1, 2, 5], [1, 3, 4], [1, 3, 5], [1, 4, 5], [2, 3, 4],
+  > Combine.comb(2,[1,2,3])
+  > [[1, 2], [1, 3], [2, 3]]
+  > Combine.comb(3,1..5 |> Enum.into([]))
+  > [[1, 2, 3], [1, 2, 4], [1, 2, 5], [1, 3, 4], [1, 3, 5], [1, 4, 5], [2, 3, 4],
    [2, 3, 5], [2, 4, 5], [3, 4, 5]]
   """
 
